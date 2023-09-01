ca3d983ad0142751b115e0748af9090b3b72d680
IRathoor <i.rathoor@gmail.comgit config --global user.name>
2023-09-01 21:19:52 +0500

    public

---
+++ IR_JS_assignment1.js
@@ -0,0 +1,67 @@
+//Assignment 1
+
+//Q1
+var userName;
+
+//Q2 
+var myName="Muhammad Ibrahim Rathore"
+alert (myName)
+
+//Q3
+var message="Hello World"
+alert (message)
+
+//Q4
+var firstName="Ali";
+var lastName="Khan";
+var fullName=""+firstName+" "+lastName+"";
+alert (fullName)
+var age="17";
+var text="Years old";
+alert (age)
+var school="SSUET";
+alert (school)
+var course="Mobile & Cloud Computing - Module A";
+alert (course)
+
+//Q5
+var food="PIZZA\nPIZZ\nPIZ\nPIZ\nPI\nP";
+alert (food)
+
+//Q6
+var inFo="I am a Software Developer, Live in Karachi with two Kids ";
+alert (inFo)
+
+//Q7
+var eMail="My email address is i.rathoor@gmail.com";
+alert (eMail)
+
+//Q8
+var book=" I am trying to learn from the book 'A smarter way to learn JavaScript'";
+alert (book)
+
+//Q9
+var message1="My name is "+firstName+" "+lastName+".";
+var message2="I am "+age+" years old";
+var message3="I study in "+school+".";
+var message4="I take "+course+" classes."
+
+//Q10
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Java Script</title>
+    <style>*{font-size: 150%;}</style>
+</head>
+<body>
+
+<div id="demo"></div>
+
+<script>
+
+document.getElementById("demo").innerHTML = "Yes, I can write HTML content through Java Script."
+</script>
+
+
+</body>
+</html>
